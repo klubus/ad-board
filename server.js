46eb2812ba544c164ec232c0ef2213496189fc26
Krystian Kluba <klubakrystian@gmail.com>
2026-02-15 11:05:13 +0100

    db

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.json());
-
 app.use(cors());
 
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  {
-    id: 2,
-    author: 'Amanda Doe',
-    text: 'They really know how to make you happy.',
-  },
-];
+mongoose.connect(process.env.DB_URL)
+  .then(() => {
+    console.log('Connected to the database');
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
-});
+    app.listen(8000, () => {
+      console.log('Server is running on port: 8000');
+    });
+  })
+  .catch(err => {
+    console.error('Database connection error:', err);
+  });
