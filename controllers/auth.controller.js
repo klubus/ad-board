3fcb4934e33f2bcd4bda6227edadbb3a73c2194f
Krystian Kluba <klubakrystian@gmail.com>
2026-02-16 22:06:38 +0100

    login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -30,4 +30,29 @@ exports.register = async (req, res) => {
   }
 };
 
-exports.login = async (req, res) => {};
+exports.login = async (req, res) => {
+  try {
+    const { login, password } = req.body;
+
+    if (
+      login &&
+      typeof login === 'string' &&
+      password &&
+      typeof password === 'string'
+    ) {
+      const user = await User.findOne({ login });
+
+      if (!user) {
+        res.status(400).send({ message: 'Login or password is incorrect' });
+      } else {
+        if (bcrypt.compareSync(password, user.password)) {
+          res.status(200).send({ message: 'Login successful' });
+        } else {
+          res.status(400).send({ message: 'Login or password is incorrect' });
+        }
+      }
+    }
+  } catch (error) {
+    res.status(500).send({ message: err.message });
+  }
+};
