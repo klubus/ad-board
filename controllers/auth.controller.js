f19f331862d658cd1dde3504ec7fc9113d2592ed
Krystian Kluba <klubakrystian@gmail.com>
2026-02-18 10:03:55 +0100

    authMiddleware

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -61,9 +61,5 @@ exports.login = async (req, res) => {
 };
 
 exports.getUser = async (req, res) => {
-  if (req.session.login) {
-    res.send({ login: req.session.login });
-  } else {
-    res.status(401).send({ message: 'You are not authorized' });
-  }
+  res.send('You are logged!');
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../controllers/auth.controller');
+const authMiddleware = require('../utils/authMiddleware');
 
 router.post('/register', auth.register);
 router.post('/login', auth.login);
-router.get('/user', auth.getUser);
+router.get('/user', authMiddleware, auth.getUser);
 
 module.exports = router;
