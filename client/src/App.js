b1e51b2c78cc9702a103c4454df0922dc028230c
Krystian Kluba <klubakrystian@gmail.com>
2026-02-19 21:09:32 +0100

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-function App() {
+import Home from './components/Home/Home.js';
+import Container from './components/Container/Container.js';
+import NavBar from './components/NavBar/NavBar.js';
+import NoMatch from './components/NoMatch/NoMatch.js';
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <main>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </Container>
+    </main>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <p>NavBar</p>
+    </>
+  );
+};
+
+export default Home;
