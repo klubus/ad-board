fe1b1277bf35c6864215a3f7ba7f8b1837b48076
Krystian Kluba <klubakrystian@gmail.com>
2026-02-22 17:54:53 +0100

    logout

--- client/src/components/Logout/Logout.js
+++ client/src/components/Logout/Logout.js
@@ -1,5 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { API_URL } from '../../config';
+import { logOut } from '../../redux/usersRedux';
+
 const Logout = () => {
-  return <div>Login</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const options = {
+      method: 'DELETE',
+    };
+
+    fetch(`${API_URL}/logout`, options).then(() => {
+      dispatch(logOut);
+    });
+  }, [dispatch]);
+
+  return null;
 };
 
 export default Logout;
--- client/src/redux/usersRedux.js
+++ client/src/redux/usersRedux.js
@@ -3,6 +3,7 @@
 // actions
 const createActionName = (actionName) => `app/users/${actionName}`;
 const LOG_IN = createActionName('LOG_IN');
+const LOG_OUT = createActionName('LOG_OUT');
 
 // action creators
 export const logIn = (payload) => ({
@@ -10,10 +11,16 @@ export const logIn = (payload) => ({
   payload,
 });
 
+export const logOut = () => ({
+  type: LOG_OUT,
+});
+
 const usersReducer = (statePart = null, action) => {
   switch (action.type) {
     case LOG_IN:
       return action.payload;
+    case LOG_OUT:
+      return null;
     default:
       return statePart;
   }
